6509df4e6c7555d892dfb5b369d6bb02f6b45e6f
HartfordClimate <hartfordclimate@gmail.com>
2017-07-21 14:31:53 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -1050,8 +1050,8 @@ $(window).on('load', function() {
     var knoxLegend = $('.polygons-legend' + legendOrder['Trees by KNOX']);
     var knox = knoxLegend.children('form');
 
-    /* Rename Public Parks into Green Spaces */
-    ppLegend.find('h6').contents().eq(1).replaceWith('Green Space');
+    /* Rename Public Parks into Landscape */
+    ppLegend.find('h6').contents().eq(1).replaceWith('Landscape');
     ppLegend.find('form').find('i').addClass('fa fa-map').css('color', 'green');
 
     knox.find('i').addClass('fa fa-circle').css('color', 'green');
